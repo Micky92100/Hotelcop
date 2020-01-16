00e3873358b66669aba0d483f883774d6967e384
Alexis DRAI <drai.alexis@gmail.com>
2020-01-16 23:47:14 +0100

    fixed some stuff

---
+++ script.js
@@ -0,0 +1,34 @@
+function imprimer_page() {
+    window.print();
+}
+
+var positionElementInPage = $('#header').offset().top;
+$(window).scroll(
+    function () {
+        if ($(window).scrollTop() > positionElementInPage) {
+
+            $('#header').addClass("fixed");
+        } else {
+
+            $('#header').removeClass("fixed");
+        }
+    }
+);
+
+$(function () {
+    $('form').submit(function (e) {
+        e.preventDefault()
+        var $form = $(this)
+        $.post($form.attr('action'), $form.serialize())
+            .done(function (data) {
+                $('#html').html(data)
+                $('#formulaire').modal('hide')
+            })
+            .fail(function () {
+                alert('ça ne marche pas...')
+            })
+    })
+    $('.modal').on('shown.bs.modal', function () {
+        $('input:first').focus()
+    })
+})
