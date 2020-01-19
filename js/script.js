02adcf47cfe572f9f377c6a4cb423b71c19da88c
Alexis DRAI <drai.alexis@gmail.com>
2020-01-19 18:39:49 +0100

    played around with jquery

--- js/script.js
+++ js/script.js
@@ -1,43 +1,69 @@
-function imprimer_page() {
-    window.print();
-}
+$(document).ready(function () {
+    console.log("ready!");
 
-var positionElementInPage = $('#header').offset().top;
-$(window).scroll(
-    function () {
-        if ($(window).scrollTop() > positionElementInPage) {
 
-            $('#header').addClass("fixed");
-        } else {
+    $(function () {
+        $('#reservation-form').submit(function (e) {
+            e.preventDefault();
+            const $results = $('#reservation-form').serializeArray();
 
-            $('#header').removeClass("fixed");
-        }
-    }
-);
-
-$(function () {
-    $('form').submit(function (e) {
-        e.preventDefault();
-        var $form = $(this);
-        $.post($form.attr('action'), $form.serialize())
-            .done(function (data) {
-                $('#html').html(data);
-                $('#formulaire').modal('hide')
-            })
-            .fail(function () {
-                alert('ça ne marche pas...')
-            })
+            if ($results[2].value === 0 || $results[2].value === "0") {
+            //    ERRORS
+            //    return;
+            } else {
+                // calcul();
+            }
+        });
     });
-    $('.modal').on('shown.bs.modal', function () {
-        $('input:first').focus()
-    })
-});
 
-$(".dropdown-menu li a").click(function () {
-    var selText = $(this).text();
-    $(this).parents('.btn-group').find('.dropdown-toggle').html(selText + ' <span class=caret></span>');
-});
 
-$( function() {
-    $( "#datepicker" ).datepicker();
-} );
+    /*$(function () {
+        $('#reservation-form').submit(function (e) {
+            console.log("hello there");
+
+            e.preventDefault();
+            var $form = $(this);
+            $.post($form.attr('action'), $form.serialize())
+                .done(function (data) {
+                    $('#html').html(data);
+                    $('#formulaire').modal('hide')
+                })
+                .fail(function () {
+                    alert('ça ne marche pas...')
+                })
+        });
+        $('.modal').on('shown.bs.modal', function () {
+            $('input:first').focus()
+        })
+    });*/
+
+    function imprimer_page() {
+        window.print();
+    }
+
+    var positionElementInPage = $('#header').offset().top;
+
+    $(window).scroll(
+        function () {
+            if ($(window).scrollTop() > positionElementInPage) {
+
+                $('#header').addClass("fixed");
+            } else {
+
+                $('#header').removeClass("fixed");
+            }
+        }
+    );
+
+    $(".dropdown-menu li a").click(function () {
+        var selText = $(this).text();
+        $(this).parents('.btn-group').find('.dropdown-toggle').html(selText + ' <span class=caret></span>');
+    });
+
+    $(function () {
+        // why is there a warning here ?
+        $("#datepicker").datepicker();
+    });
+
+
+});
