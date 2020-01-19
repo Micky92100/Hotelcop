1255f27727dee82b6c243872f45bd9e677364e22
abdelhafid <abdelhafid.arbaoui@gmail.com>
2020-01-19 16:28:46 +0100

    initial commit

--- js/script.js
+++ js/script.js
@@ -15,25 +15,6 @@ $(window).scroll(
     }
 );
 
-$(function () {
-    $('form').submit(function (e) {
-        e.preventDefault();
-        var $form = $(this);
-        $.post($form.attr('action'), $form.serialize())
-            .done(function (data) {
-                $('#html').html(data);
-                $('#formulaire').modal('hide')
-            })
-            .fail(function () {
-                alert('ça ne marche pas...')
-            })
-    });
-    $('.modal').on('shown.bs.modal', function () {
-        $('input:first').focus()
-    })
-});
-
-$(".dropdown-menu li a").click(function () {
-    var selText = $(this).text();
-    $(this).parents('.btn-group').find('.dropdown-toggle').html(selText + ' <span class=caret></span>');
-});
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
